e1c571f71e7f4215530d03228ffcfffca44560fb
shaxzod67 <uzbuz741@gmail.com>
2024-06-10 15:14:43 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+
+// import './App.css'
+
+import Footer from "./Footer/footer"
+import Header from "./pages/Header/header"
+import Main from "./pages/Main/main"
+
+function App() {
+
+  return (
+    <div >
+      <header class="text-gray-400 bg-gray-900 body-font fixed  z-10 w-[100%]">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <span class="ml-3 text-xl">Qurilish</span>
+          </a>
+          <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+            <a class="mr-5 hover:text-white">Home</a>
+            <a class="mr-5 hover:text-white">About </a>
+            <a class="mr-5 hover:text-white">Contact</a>
+            <a class="mr-5 hover:text-white">Footer</a>
+          </nav>
+          <button class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Come
+            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+              <path d="M5 12h14M12 5l7 7-7 7"></path>
+            </svg>
+          </button>
+        </div>
+      </header>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/pages/Main/main.jsx
@@ -0,0 +1,47 @@
+import rasm1 from '../../image/rasm1.webp'
+import rasm2 from '../../image/rasm2.webp'
+import rasm3 from '../../image/rasm3.webp'
+import rasm6 from '../../image/rasm6.webp'
+import rasm4 from '../../image/rasm4.webp'
+import rasm5 from '../../image/rasm5.webp'
+
+function Main() {
+  return (
+    <div>
+        <section class="text-gray-400 bg-gray-900 body-font">
+  <div class="container px-5 py-24 mx-auto flex flex-wrap">
+    <div class="flex w-full mb-20 flex-wrap">
+      <h1 class="sm:text-3xl text-2xl font-medium title-font text-white lg:w-1/3 lg:mb-0 mb-4">Bizning Tayyor uylar</h1>
+      <p class="lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom.</p>
+    </div>
+    <div class="flex flex-wrap md:-m-2 -m-1">
+      <div class="flex flex-wrap w-1/2">
+        <div class="md:p-2 p-1 w-1/2">
+          <img alt="gallery" class="w-full object-cover h-full object-center block" src={rasm6}/>
+        </div>
+        <div class="md:p-2 p-1 w-1/2">
+          <img alt="gallery" class="w-full object-cover h-full object-center block" src={rasm2}/>
+        </div>
+        <div class="md:p-2 p-1 w-full">
+          <img alt="gallery" class="w-full h-full object-cover object-center block" src={rasm1}/>
+        </div>
+      </div>
+      <div class="flex flex-wrap w-1/2">
+        <div class="md:p-2 p-1 w-full">
+          <img alt="gallery" class="w-full h-full object-cover object-center block" src={rasm3}/>
+        </div>
+        <div class="md:p-2 p-1 w-1/2">
+          <img alt="gallery" class="w-full object-cover h-full object-center block" src={rasm4}/>
+        </div>
+        <div class="md:p-2 p-1 w-1/2">
+          <img alt="gallery" class="w-full object-cover h-full object-center block" src={rasm5}/>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+    </div>
+  )
+}
+
+export default Main
